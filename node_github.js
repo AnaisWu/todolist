53dba5091e7018895445b9c59ce88f229b4e6e84
AnaisWu <78689790+AnaisWu@users.noreply.github.com>
2021-02-22 21:10:11 +0100

    Delete node_github.js

--- node_github.js
+++
@@ -1,8 +0,0 @@
-let express = require('express')
-let app = express();
-let port = 8000;
-
-app.get('/',(request,response) => response.send('Hello Wolrd !!!'));
-
-app.listen(port,function(){ console.log("Server running on port" + port);
-})
